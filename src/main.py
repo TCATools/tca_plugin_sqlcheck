1cfed63c2d27b87c4b7c7f81720e7d08d7c8667e
cyw3 <2927096163@qq.com>
2022-08-06 17:28:12 +0800

    :art: update rule name spilt
    Signed-off-by: cyw3 <2927096163@qq.com>

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@
 # ==============================================================================
 
 
+import re
 import os
 import sys
 import json
@@ -212,13 +213,13 @@ class SQLCheck(object):
                 msg.append(line)
         return issues
 
-    def __convert(self, one_string, space_character=" "):
+    def __convert(self, one_string, space_character=" |-"):
         """
         one_string:输入的字符串
         space_character:字符串的间隔符，以其做为分隔标志
         """
         # 将字符串转化为list
-        string_list = str(one_string).split(space_character)
+        string_list = re.split(space_character, str(one_string))
         # str.capitalize():将字符串的首字母转化为大写
         others_capital = [word.capitalize() for word in string_list]
         # 将list组合成为字符串，中间无连接符。
